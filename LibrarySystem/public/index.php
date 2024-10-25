58e2a00459b5341d1904b4488eee69d02968907f
sorkevinick <131691793+sorkevinick@users.noreply.github.com>
2024-10-25 18:55:59 -0300

    Update index.php

--- LibrarySystem/public/index.php
+++ LibrarySystem/public/index.php
@@ -5,13 +5,10 @@ use LibrarySystem\Repositories\BookRepository;
 use LibrarySystem\Services\BorrowService;
 use LibrarySystem\Sessions\SessionManager;
 
-// Dependencies
 $bookRepository = new BookRepository();
 $sessionManager = new SessionManager();
 
-// Borrow Service
 $borrowService = new BorrowService($bookRepository, $sessionManager);
 
-// Borrow and return books
 $borrowService->borrowBook('1984', 1);
 $borrowService->returnBook('1984', 1);
